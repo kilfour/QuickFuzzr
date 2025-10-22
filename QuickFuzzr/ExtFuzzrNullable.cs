000d286354b74efb67447a7e6b9df4060f448eb3
kilfour <kilfour@gmail.com>
2025-10-22 19:31:10 +0200

    nullable percentageOfNulls

--- QuickFuzzr/ExtFuzzrNullable.cs
+++ QuickFuzzr/ExtFuzzrNullable.cs
@@ -7,38 +7,38 @@ public static partial class ExtFuzzr
 	public static FuzzrOf<T?> Nullable<T>(this FuzzrOf<T> generator)
 		where T : struct
 	{
-		return Nullable(generator, 5);
+		return Nullable(generator, 0.2);
 	}
 
-	public static FuzzrOf<T?> Nullable<T>(this FuzzrOf<T> generator, int timesBeforeResultIsNullAproximation)
+	public static FuzzrOf<T?> Nullable<T>(this FuzzrOf<T> generator, double percentageOfNulls)
 		where T : struct
 	{
 		return
-			s =>
+			state =>
 			{
-				if (s.Random.Next(0, timesBeforeResultIsNullAproximation) == 0)
-					return new Result<T?>(null, s);
-				var val = generator(s).Value;
-				return new Result<T?>(val, s);
+				if (state.Random.NextDouble() < percentageOfNulls)
+					return new Result<T?>(null, state);
+				var val = generator(state).Value;
+				return new Result<T?>(val, state);
 			};
 	}
 
 	public static FuzzrOf<T?> NullableRef<T>(this FuzzrOf<T> generator)
 		where T : class
 	{
-		return NullableRef(generator, 5);
+		return NullableRef(generator, 0.2);
 	}
 
-	public static FuzzrOf<T?> NullableRef<T>(this FuzzrOf<T> generator, int timesBeforeResultIsNullAproximation)
+	public static FuzzrOf<T?> NullableRef<T>(this FuzzrOf<T> generator, double percentageOfNulls)
 		where T : class
 	{
 		return
-			s =>
+			state =>
 			{
-				if (s.Random.Next(0, timesBeforeResultIsNullAproximation) == 0)
-					return new Result<T?>(null, s);
-				var val = generator(s).Value;
-				return new Result<T?>(val, s);
+				if (state.Random.NextDouble() < percentageOfNulls)
+					return new Result<T?>(null, state);
+				var val = generator(state).Value;
+				return new Result<T?>(val, state);
 			};
 	}
 
@@ -50,8 +50,14 @@ public static partial class ExtFuzzr
 			{
 
 				var val = generator(s).Value;
+				var i = 0;
 				while (val == null)
+				{
 					val = generator(s).Value;
+					i++;
+					if (i >= 50)
+						throw new HeyITriedFiftyTimesButCouldNotGetADifferentValue();
+				}
 				return new Result<T?>(val, s);
 			};
 	}
