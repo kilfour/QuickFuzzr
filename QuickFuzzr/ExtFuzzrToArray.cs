fffbce4e3d17717934e9bfa26245af0fec1cd0a0
kilfour <kilfour@gmail.com>
2026-03-21 11:20:33 +0100

    Added `.ToArray()`.

---
+++ QuickFuzzr/ExtFuzzrToArray.cs
@@ -0,0 +1,11 @@
+﻿namespace QuickFuzzr;
+
+public static partial class ExtFuzzr
+{
+    /// <summary>
+    /// Creates a Fuzzr that materializes the sequence produced by the source Fuzzr as an array.
+    /// Use when you need fixed-size collection semantics, array APIs, or eager materialization from generated enumerable data.
+    /// </summary>
+    public static FuzzrOf<T[]> ToArray<T>(this FuzzrOf<IEnumerable<T>> fuzzr)
+        => from elements in fuzzr select elements.ToArray();
+}
